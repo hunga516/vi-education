c75516691f8f1168cee0ddc313340acf5e77c94c
hunga516 <hunga5160@gmail.com>
2024-09-10 23:58:42 +0700

    update create post ui

--- src/components/Modal/CreatePostModal.js
+++ src/components/Modal/CreatePostModal.js
@@ -1,71 +1,54 @@
-import { IoArrowBack } from "react-icons/io5";
 import ReactDOM from 'react-dom';
+
+
+import { IoArrowBack } from "react-icons/io5";
+
+import Button from '../Button';
 
 
 function CreatePostModal({ toggleIsShowCreatePost }) {
     return ReactDOM.createPortal(
         <div className="relative">
+            {/* Wrapper Disable */}
             <div className="fixed h-[100vh] inset-0 bg-gray-500 opacity-75 z-20">
             </div>
+
+            {/* Modal */}
             <div className="fixed flex justify-center items-center inset-0 z-20">
                 <div className="overflow-auto overscroll-y-contain h-[80vh] w-[50vw] bg-white rounded-xl">
                     <div className="relative px-12 py-4">
-                        <button
-                            className="flex items-center gap-2 sticky top-0 h-16 w-full bg-white "
-                            onClick={() => toggleIsShowCreatePost()}
+                        <div
+                            className="back-action flex justify-between items-center gap-2 sticky top-0 h-16 w-full bg-white "
                         >
-                            <IoArrowBack />
-                            <button className="text-base font-semibold text-gray-700 leading-9">Trở về</button>
-                        </button>
+                            <button onClick={toggleIsShowCreatePost} className="flex items-center gap-2">
+                                <IoArrowBack />
+                                <h2 className="text-base font-semibold text-gray-700 leading-9">Trở về</h2>
+                            </button>
+
+                            <div className="container-action flex items-center gap-2">
+                                <Button size='medium' type='outline-dark'>
+                                    Lưu nháp
+                                </Button>
+                                <Button className="px-4" type='primary'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                    Đăng bài
+                                </Button>
+                            </div>
+                        </div>
                         <div class="border-b border-gray-900/10 pb-12 mt-4">
                             <h2 class="text-base font-semibold leading-7 text-gray-900">Tạo bài viết</h2>
                             <p class="mt-1 text-sm leading-6 text-gray-600">Chia sẽ kiến thức của bạn hoặc hỏi đáp về các vấn đề bạn gặp phải.</p>
-
-                            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                                <div class="sm:col-span-4">
-                                    <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
-                                    <div class="mt-2">
-                                        <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                            <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
-                                            <input type="text" name="username" id="username" autocomplete="username" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="janesmith" />
-                                        </div>
+                            <div className='flex mt-8 gap-6'>
+                                <div className='flex flex-row gap-2'>
+                                    <div className='topic-input flex flex-col gap-2'>
+                                        <label htmlFor='topic' className='text-sm font-medium text-gray-900 leading-6'>Chủ đề</label>
+                                        <input type='text' id='topic' className='py-1.5 text-sm font-medium leading-6 text-gray-900 ring-1 ring-inset ring-gray-300 rounded-md p-2 placeholder:text-gray-400 placeholder:font-medium placeholder:text-sm' placeholder='Nhập chủ đề' />
                                     </div>
-                                </div>
-
-                                <div class="col-span-full">
-                                    <label for="about" class="block text-sm font-medium leading-6 text-gray-900">About</label>
-                                    <div class="mt-2">
-                                        <textarea id="about" name="about" rows="8" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
-                                    </div>
-                                    <p class="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
-                                </div>
-
-                                <div class="col-span-full">
-                                    <label for="photo" class="block text-sm font-medium leading-6 text-gray-900">Photo</label>
-                                    <div class="mt-2 flex items-center gap-x-3">
-                                        <svg class="h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                                            <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
-                                        </svg>
-                                        <button type="button" class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Change</button>
-                                    </div>
-                                </div>
-
-                                <div class="col-span-full">
-                                    <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900">Cover photo</label>
-                                    <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                                        <div class="text-center">
-                                            <svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                                                <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
-                                            </svg>
-                                            <div class="mt-4 flex text-sm leading-6 text-gray-600">
-                                                <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
-                                                    <span>Upload a file</span>
-                                                    <input id="file-upload" name="file-upload" type="file" class="sr-only" />
-                                                </label>
-                                                <p class="pl-1">or drag and drop</p>
-                                            </div>
-                                            <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
-                                        </div>
+                                    <div className='title-input flex flex-col gap-2'>
+                                        <label htmlFor='title' className='text-sm font-medium text-gray-900 leading-6'>Tiêu đề</label>
+                                        <input type='text' id='title' className='py-1.5 text-sm font-medium leading-6 text-gray-900 ring-1 ring-inset ring-gray-300 rounded-md p-2' placeholder='Nhập chủ đề' />
                                     </div>
                                 </div>
                             </div>
