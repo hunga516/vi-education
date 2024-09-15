4405ac8e93ab85e7448758b0a7782cd9881c8979
Ngoc Loc <hunga5160@gmail.com>
2024-09-15 20:59:35 +0700

    change background

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,7 +2,7 @@ const images = {
     logo: require('./logo.svg').default,
     sony: require('./img/sony.png'),
     logoEducation: require('./img/logoEducation.png'),
-    background: require('./img/background.jpg')
+    background: require('./img/background.jpeg')
 }
 
 export default images;
